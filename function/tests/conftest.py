3ecfc3a22947bd6dadfe14f6455940e1c1288082
Ray Kohler <rkohler@renovo1.com>
2025-08-11 14:27:44 -0400

    feat: Add LRU cache to reduce boto3 calls
    fixes: #919
    
    chore: clean up lint warnings

--- function/tests/conftest.py
+++ function/tests/conftest.py
@@ -1,20 +1,23 @@
-import pytest
+"""Configuration for testing."""
 import os
+import pytest
 
 @pytest.fixture(autouse=True)
 def set_env_vars():
-  os.environ['LETSENCRYPT_EMAIL'] = 'email@example.com'
-  os.environ['LETSENCRYPT_DOMAINS'] = 'example.com'
-  os.environ['PREFERRED_CHAIN'] = 'ISRG Root X1'
-  os.environ['CERTIFICATE_BUCKET'] = 'example-cert-bucket'
-  os.environ['CERTIFICATE_STORAGE'] = 's3'
-  os.environ['NOTIFICATION_SNS_ARN'] = 'arn:aws:sns:us-east-1:123456789012:example-topic'
-  os.environ['REISSUE_DAYS'] = '30'
-  os.environ['DRY_RUN'] = 'False'
-  os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
-  os.environ['AWS_REGION'] = 'us-east-1'
-  os.environ['AWS_ACCESS_KEY_ID'] = 'fake'
-  os.environ['AWS_SECRET_ACCESS_KEY'] = 'fake'
-  os.environ['OBJECT_PREFIX'] = ''
-  os.environ['EFS_PATH'] = '/tmp/efs'
-  yield
+    """Set environment variables for testing."""
+    os.environ['LETSENCRYPT_EMAIL'] = 'email@example.com'
+    os.environ['LETSENCRYPT_DOMAINS'] = 'example.com'
+    os.environ['PREFERRED_CHAIN'] = 'ISRG Root X1'
+    os.environ['CERTIFICATE_BUCKET'] = 'example-cert-bucket'
+    os.environ['CERTIFICATE_STORAGE'] = 's3'
+    os.environ['KEY_TYPE'] = 'ecdsa'
+    os.environ['NOTIFICATION_SNS_ARN'] = 'arn:aws:sns:us-east-1:123456789012:example-topic'
+    os.environ['REISSUE_DAYS'] = '30'
+    os.environ['DRY_RUN'] = 'False'
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+    os.environ['AWS_REGION'] = 'us-east-1'
+    os.environ['AWS_ACCESS_KEY_ID'] = 'fake'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'fake'
+    os.environ['OBJECT_PREFIX'] = ''
+    os.environ['EFS_PATH'] = '/tmp/efs'
+    yield
